ebb42b4097fb0acb0b66e0ddbd315973297f16c3
Thato Omphile Nameng <71035775+Thato-Nameng@users.noreply.github.com>
2024-09-22 12:20:40 +0000

    Add files via upload

---
+++ ABCRetail/Models/CustomerFileLogViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ABCRetail.Models
+{
+    public class CustomerFileLogViewModel
+    {
+        public string CustomerName { get; set; }
+        public string CustomerFilesContent { get; set; } 
+        public string CustomerLogsContent { get; set; }  
+    }
+}
---
+++ ABCRetail/Models/ProductModel.cs
@@ -0,0 +1,21 @@
+﻿using Azure.Data.Tables;
+using Azure;
+using System.ComponentModel.DataAnnotations;
+
+namespace ABCRetail.Models
+{
+    public class ProductModel : ITableEntity
+    {
+        public string PartitionKey { get; set; } = "Products";
+        public string RowKey { get; set; } = Guid.NewGuid().ToString();
+        public string ProductId { get; set; } = Guid.NewGuid().ToString();
+        public string ProductName { get; set; } = string.Empty;
+        public double Price { get; set; }
+        public int Quantity { get; set; } 
+        public string ImageUrl { get; set; } = string.Empty;
+        public DateTime CreatedDate { get; set; }
+
+        public ETag ETag { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+    }
+}
